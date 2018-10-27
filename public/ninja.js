a14407526de0f3f84e35da064d780f1edbaf3a67
KaushikIyer16 <kaushiiyer@gmail.com>
2018-10-28 02:25:43 +0530

    added ninja sprites

---
+++ public/ninja.js
@@ -0,0 +1,25 @@
+let ninja;
+
+function preloadNinja(context){
+  context.load.multiatlas('ninja', 'static/assets/ninja_run.json', 'static/assets/');
+  context.load.multiatlas('ninjaFly', 'static/assets/ninja_fly.json', 'static/assets');
+}
+
+function initNinjaSprites(context, pos){
+  let ninja = context.add.sprite(pos.x, pos.y, 'ninja', '1.png');
+
+  let runFrames = context.anims.generateFrameNames('ninja', {
+                         start: 1, end: 8, zeroPad: 0,
+                         prefix: '', suffix: '.png'
+                     });
+
+  let flyFrames = context.anims.generateFrameNames('ninjaFly',{
+                        start: 1, end: 7, zeroPad: 0,
+                        prefix: '', suffix: '.png'
+                  });
+
+  context.anims.create({key: 'run', frames: runFrames, frameRate: 10, repeat: -1});
+  context.anims.create({key: 'fly', frames: flyFrames, frameRate: 10, repeat: -1});
+
+  return ninja;
+}
