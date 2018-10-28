4e59e0c125c237c0809bead6490de9ee62308090
aulisius <aulisius7@gmail.com>
2018-10-28 20:47:50 +0530

    make port dynamic

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require('express');
 const app = express();
-const port = 3000;
+let { PORT = 3000 } = process.env;
 
 app.use('/', express.static(path.join(__dirname, 'public')));
-app.listen(port, () => console.log(`http://localhost:${port}/`));
+app.listen(port, () => console.log(`http://localhost:${PORT}/`));
